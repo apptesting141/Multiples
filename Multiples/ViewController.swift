54ccec71552a041275f2fe33ec97de04e0e59e7b
Suvendu Jana <suvendujana@Suvendus-MacBook-Pro.local>
2016-01-23 16:35:50 +0530

    added files for Multiples

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,8 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headingLabel: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    var sum = 0;
+    
+    @IBAction func play(sender: AnyObject) {
+
+        var preSum = 0
+        preSum = sum
+        super.viewDidLoad()
+        resultLabel.hidden = false
+        headingLabel.hidden = true
+        textField.hidden = true
+        
+        sum = sum + Int(textField.text!)!
+        
+        
+        resultLabel.text = "\(preSum)! + \(textField.text)!  = \(sum)!"
+        
+        print(resultLabel.text)
+
+        
+        
+    }
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultLabel.hidden = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
